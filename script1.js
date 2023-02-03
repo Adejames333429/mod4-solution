1717783a9aa0ed31a0f3d426d00e98a5d5884c11
James. H <95620916+Adejames333429@users.noreply.github.com>
2023-02-04 02:22:58 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,6 @@
+var name = "Joe";
+function sayhello () {
+    console.log("Goodbye " + name);
+}
+
+sayhello();
---
+++ script2.js
@@ -0,0 +1,6 @@
+var name = "Johnson";
+function  sayHi ()  {
+    console.log("Hello " + name);
+}
+
+sayHi();
